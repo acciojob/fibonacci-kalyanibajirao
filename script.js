47ab95dc9f1e8ea70f321fc811e598448de0ed44
Kalyani Bajirao <95849305+kalyanibajirao@users.noreply.github.com>
2024-05-15 12:53:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function fibonacci(num) {
 // your code here
-	if (num <= 1) {
-    return num;
-  } else {
-    return fibonacci(num - 1) + fibonacci(num - 2);
+	if (num === 1) {
+    return 0;
   }
+  if (num === 2) {
+    return 1;
+  }
+  let sequence = [0, 1];
+  for (let i = 2; i < num; i++) {
+    sequence[i] = sequence[i - 1] + sequence[i - 2];
+  }
+  return sequence[num - 1];
 }
 
 module.exports = fibonacci;
